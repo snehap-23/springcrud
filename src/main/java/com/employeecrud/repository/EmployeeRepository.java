ca13f9e003d7d08d66d773da697f4d725b619f4e
Pravinp-20 <112651259+Pravinp-20@users.noreply.github.com>
2025-08-03 12:30:49 +0530

    initial commit

---
+++ src/main/java/com/employeecrud/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.employeecrud.repository;
+
+import com.employeecrud.doa.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+}
